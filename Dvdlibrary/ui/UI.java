36757c98a4c6c6149fc89a3ca0ef5fa983e9322b
nishajha612 <105644740+nishajha612@users.noreply.github.com>
2022-06-28 12:12:12 +0100

    Update UI.java

--- Dvdlibrary/ui/UI.java
+++ Dvdlibrary/ui/UI.java
@@ -4,15 +4,7 @@ public interface UI {
 
     public void say(String message);
 
-    /**
-     * Reads a String from the user
-     * @return the String
-     */
-    public String readString();
+  public String readString();
 
-    /**
-     * Reads a double from the user
-     * @return the double
-     */
-    public double readDouble();
+     public double readDouble();
 }
