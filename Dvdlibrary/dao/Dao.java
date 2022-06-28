67bf6763aae12d70c6d3809c6ffef469932910df
nishajha612 <105644740+nishajha612@users.noreply.github.com>
2022-06-28 12:07:45 +0100

    Update Dao.java

--- Dvdlibrary/dao/Dao.java
+++ Dvdlibrary/dao/Dao.java
@@ -2,34 +2,11 @@ package Dvdlibrary.dao;
 import Dvdlibrary.dto.Dvd;
 
 public interface Dao {
-
-    public void addDvd(Dvd dvd);
-
-    /**
-     * Removes a DVD from the library
-     * @param name the title of the DVD to find and remove
-     */
-    public void removeDvd(String name);
-
-    /**
-     * Gets a DVD given the name
-     * @param name the title of the DVD to find
-     * @return the DVD
-     */
-    public Dvd getDvd(String name);
-
-    /**
-     * Loads the library of DVDs
-     */
+public void addDvd(Dvd dvd);
+public void removeDvd(String name);
+public Dvd getDvd(String name);
     public void load();
-
-    /**
-     * Saves the library of DVDs
-     */
     public void save();
 
-    /**
-     * Gets the titles of all DVDs
-     */
-    public String[] getTitles();
+   public String[] getTitles();
 }
