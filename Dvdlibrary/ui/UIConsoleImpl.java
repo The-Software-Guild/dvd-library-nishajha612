66660d36a891fd0fec7f96c38a9491e127717436
nishajha612 <105644740+nishajha612@users.noreply.github.com>
2022-06-28 12:12:44 +0100

    Update UIConsoleImpl.java

--- Dvdlibrary/ui/UIConsoleImpl.java
+++ Dvdlibrary/ui/UIConsoleImpl.java
@@ -2,20 +2,13 @@ package Dvdlibrary.ui;
 
 import java.util.Scanner;
 public class UIConsoleImpl implements UI {
-    /**
-     * Displays a message to the user
-     * @param message the message to send to the user
-     */
+    
     @Override
     public void say(String message) {
         System.out.println(message);
     }
 
-    /**
-     * Reads a String from the user
-     * @return the String
-     */
-    @Override
+   @Override
     public String readString() {
         Scanner inputReader;
         String input;
@@ -25,11 +18,6 @@ public class UIConsoleImpl implements UI {
         input = inputReader.nextLine();
         return input;
     }
-
-    /**
-     * Reads a double from the user
-     * @return the double
-     */
     @Override
     public double readDouble() {
         Scanner inputReader;
